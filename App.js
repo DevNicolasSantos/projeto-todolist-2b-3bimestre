87aa6569f921b573cea027f05e3c6a6fd4e93ddb
henzo lucas rizzo de souza <henzinho620@gmail.com>
2025-08-15 11:08:23 -0300

    [feat] Style of App.js

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import logo from './assets/logo.png';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <View>
-        <Image source={list} />
+        <Image source={logo} style={styles.logo}/>
       </View>
       <Text>Projeto 2B - 13/08/2025</Text>
       <StatusBar style="auto" />
@@ -19,5 +20,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  logo: {
+    width: 128,
+    height: 128,
   },
 });
